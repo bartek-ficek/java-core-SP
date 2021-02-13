714a1a596040dc341084039e7823e8cb9a8fb56d
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-02-13 13:14:57 +0100

    4.Arrays - Homework 4 - created ChessPiece enum

---
+++ src/pl/bartek/ficek/arrays/homework/d/ChessPiece.java
@@ -0,0 +1,20 @@
+package pl.bartek.ficek.arrays.homework.d;
+
+public enum ChessPiece {
+
+    EMPTY,
+
+    B_PAWN,
+    B_ROOK,
+    B_BISHOP,
+    B_KNIGHT,
+    B_QUEEN,
+    B_KING,
+
+    W_PAWN,
+    W_ROOK,
+    W_BISHOP,
+    W_KNIGHT,
+    W_QUEEN,
+    W_KING
+}
---
+++ src/pl/bartek/ficek/arrays/homework/d/ChestBoard.java
@@ -0,0 +1,7 @@
+package pl.bartek.ficek.arrays.homework.d;
+
+public class ChestBoard {
+
+    private ChessPiece[][] board = new ChessPiece[8][8];
+
+}
