0fe56214c78328655848f222223b5855b66fe87e
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-02-14 05:03:26 +0100

    5.Loops&Conditionals - Homework 3 DONE in firsty way

--- src/pl/bartek/ficek/loops_conditionals/homework/HomeworksResults.java
+++ src/pl/bartek/ficek/loops_conditionals/homework/HomeworksResults.java
@@ -1,21 +1,26 @@
 package pl.bartek.ficek.loops_conditionals.homework;
 
-import pl.bartek.ficek.loops_conditionals.homework.a.LoopA;
-import pl.bartek.ficek.loops_conditionals.homework.b.LoopB;
+import pl.bartek.ficek.loops_conditionals.homework.c.LoopC;
 
 public class HomeworksResults {
 
     private static final String BREAKER = "**********************";
 
     public static void main(String[] args) {
+//
+//        //Exercise a solution:
+//        LoopA.writeNumbers();
+//
+//        System.out.println(BREAKER);
+//
+//        //Exercise b solution:
+//        LoopB.writeNumbers();
+//
+//        System.out.println(BREAKER);
 
-        //Exercise a solution:
-        LoopA.writeNumbers();
 
-        System.out.println(BREAKER);
-
-        //Exercise b solution:
-        LoopB.writeNumbers();
+        //Exercise c solution:
+        LoopC.writeNumbers1();
 
         System.out.println(BREAKER);
 
---
+++ src/pl/bartek/ficek/loops_conditionals/homework/c/LoopC.java
@@ -0,0 +1,10 @@
+package pl.bartek.ficek.loops_conditionals.homework.c;
+
+public class LoopC {
+
+    public static void writeNumbers1() {
+        for (int i = -9; i <= 40 ; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
